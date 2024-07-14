c1ed61778067152a0f98ad0c28a98871e4e27880
icysamon <icysamon@gmail.com>
2024-07-14 18:48:24 +0900

    feat: planet assets

--- scripts/planet.gd
+++ scripts/planet.gd
@@ -1,13 +1,17 @@
 extends Node2D
 
 const TWEEN_SCALE_TIME = 0.1
-const SPEED_ROTATION : float = 0.5
+
+@onready var SPRITE2D = $Sprite2D
+@export var TEXTURE : Texture
+@export var SPEED_ROTATION : float = 0.5
 
 var default_scale : Vector2 = Vector2.ONE
 var tween_size_add : Vector2 = Vector2.ONE * 0.2
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	default_scale = scale
+	SPRITE2D.texture = TEXTURE
 
 	
 func _physics_process(delta):
