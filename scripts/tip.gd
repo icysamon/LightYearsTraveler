bd2b1053ba5da904e66d80657588e3be7b1397dd
icysamon <icysamon@gmail.com>
2024-07-16 14:06:49 +0900

    refactor: tip scene

--- scripts/tip.gd
+++ scripts/tip.gd
@@ -1,20 +1,31 @@
-extends Control
+extends TextureRect
 
+# tween parameter
 const TWEEN_SCALE_TIME = 0.2
 
+# label parameter
 var event_name
 var event_description
 
+
 func _ready():
+	# tip visable init
 	if modulate.a != 0:
 		modulate.a = 0
-		
+	
+	# display tip with tween
 	var tween = get_tree().create_tween()
 	tween.tween_property(self, "modulate:a", 1, TWEEN_SCALE_TIME)
 
+
 func _process(delta):
-	$TextureRect/Name.text = event_name
-	$TextureRect/Description.text = event_description
+	# update label
+	$Name.text = event_name
+	$Description.text = event_description
+	return delta
+
+
 func _on_timer_timeout():
+	# hide the tip after a certain period of time
 	var tween = get_tree().create_tween()
 	tween.tween_property(self, "modulate:a", 0, TWEEN_SCALE_TIME)
