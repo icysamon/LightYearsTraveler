667a9e287eb6f2db6dbf9e6b02b1f56ad5a3ebbd
icysamon <icysamon@gmail.com>
2024-07-19 11:50:54 +0900

    feat: global button sfx

--- scripts/buttons/back_to_menu.gd
+++ scripts/buttons/back_to_menu.gd
@@ -1,4 +1,10 @@
 extends Button
 @onready var PATH = "res://scene/main/menu.tscn"
+@export var sfx: AudioStreamPlayer
 func _on_button_down():
 	get_tree().change_scene_to_file(PATH)
+
+
+func _on_mouse_entered():
+	sfx.play()
+	pass # Replace with function body.
--- scripts/buttons/restart.gd
+++ scripts/buttons/restart.gd
@@ -1,3 +1,9 @@
 extends Button
+@export var sfx: AudioStreamPlayer
 func _on_button_down():
 	get_tree().reload_current_scene()
+
+
+func _on_mouse_entered():
+	sfx.play()
+	pass # Replace with function body.
