6a8e254a0c5fc5d1331ffb011609bbbffe182a49
icysamon <icysamon@gmail.com>
2024-07-19 14:01:05 +0900

    feat: bullet

---
+++ scripts/bullet/bullet.gd
@@ -0,0 +1,20 @@
+extends Sprite2D
+
+var target_position : Vector2 = Vector2.ZERO
+var dir : Vector2 = Vector2.ZERO
+const speed : int = 180
+
+
+func _ready():
+	dir = (target_position - position).normalized()
+	pass
+
+
+func _physics_process(delta):
+	position += dir * speed * delta
+
+func _on_area_2d_area_entered(area):
+	if area.area_type == "player":
+		area.owner.stamina -= 1
+		pass
+	pass # Replace with function body.
--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,12 +1,22 @@
 extends Sprite2D
 
 const SPEED = 50
+@onready var BULLET = preload("res://scene/child/bullet.tscn")
 
 var dir = Vector2.ZERO
 
 func _process(delta):
 	dir = (get_tree().get_nodes_in_group("player")[0].position - position).normalized()
+	return delta
 	
-	pass
+
 func _physics_process(delta):
 	position += dir * SPEED * delta
+
+
+func _on_timer_timeout():
+	var bullet = BULLET.instantiate()
+	bullet.target_position = get_tree().get_nodes_in_group("player")[0].position
+	add_child(bullet)
+	print(1)
+
