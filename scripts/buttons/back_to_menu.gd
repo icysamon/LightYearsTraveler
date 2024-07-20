bd99aa9a2b82fdc02e230cb200a2024f683223a8
icysamon <icysamon@gmail.com>
2024-07-20 22:49:32 +0900

    fix: music; balance

--- scripts/buttons/back_to_menu.gd
+++ scripts/buttons/back_to_menu.gd
@@ -4,9 +4,10 @@ extends Button
 func _on_button_down():
 	AudioStreamPlayer2d.flag_loop = false
 	AudioStreamPlayer2d.flag_main_music = false
-	AudioStreamPlayer2d.stop()
-	AudioStreamPlayer2d.stream = AudioStreamPlayer2d.menu_music
-	AudioStreamPlayer2d.play()
+	if AudioStreamPlayer2d.stream != AudioStreamPlayer2d.menu_music:
+		AudioStreamPlayer2d.stop()
+		AudioStreamPlayer2d.stream = AudioStreamPlayer2d.menu_music
+		AudioStreamPlayer2d.play()
 	get_tree().change_scene_to_file(PATH)
 
 
